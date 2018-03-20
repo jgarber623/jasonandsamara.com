5194e769053b44f0f5f56d0d1df5ed7e38e0c0cd
Jason Garber <jason@sixtwothree.org>
2018-03-20 16:21:49 -0400

    Update to Ruby 2.4.3, remove Capistrano config, update gems

--- config/deploy.rb
+++
@@ -1,8 +0,0 @@
-# config valid only for current version of Capistrano
-lock '3.10.1'
-
-set :application, 'jasonandsamara.com'
-set :repo_url, 'git@github.com:jgarber623/jasonandsamara.com.git'
-set :chruby_ruby, 'ruby-2.4.2'
-
-before 'deploy:publishing', :build
